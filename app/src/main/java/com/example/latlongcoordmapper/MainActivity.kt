49c5b098485bec52143e8430dd3c7555222f7fb9
Parker Walters <parkerrwalters@gmail.com>
2024-04-14 18:19:03 -0700

    rabbit hole of downloding, permissions struggles, gonna restart

--- app/src/main/java/com/example/latlongcoordmapper/MainActivity.kt
+++ app/src/main/java/com/example/latlongcoordmapper/MainActivity.kt
@@ -1,25 +1,48 @@
 package com.example.latlongcoordmapper
 
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.TextUtils
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.coordinatorlayout.widget.CoordinatorLayout
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.snackbar.Snackbar
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.io.File
+import java.io.FileOutputStream
+import java.io.OutputStream
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var dataList: MutableList<Cord>
+    private lateinit var dataManager: DataManager
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val cordListRV: RecyclerView = findViewById(R.id.rv_cord_list)
         val cordEntryET = findViewById<EditText>(R.id.et_cord_entry)
         val addCordBtn = findViewById<Button>(R.id.btn_add_cord)
+        val saveDataBtn = findViewById<Button>(R.id.btn_save_data)
         val coordinatorLayout = findViewById<CoordinatorLayout>(R.id.coordinator_layout)
+
+//        if(ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+//            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE), 101)
+//        }
 
         //val cordList = mutableListOf<String>()
 
@@ -74,7 +97,66 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
+//        suspend fun saveDataToFile(data: MutableList<Cord>, file: File) {
+//            withContext(Dispatchers.IO) {
+//                file.writeText(data.toCsvString())
+//            }
+//        }
+//        fun OutputStream.writeCsv(cords: MutableList<Cord>){
+//            val writer = bufferedWriter()
+//            writer.write("\"\"\"Lat\", \"Long\"\"\"")
+//            writer.newLine()
+//            cords.forEach{
+//                writer.write("${it.id},${it.text}\"")
+//                writer.newLine()
+//            }
+//            writer.flush()
+//        }
 
-        //cordListTV.text = "Lat=0.0 Long=0.0"
+        saveDataBtn.setOnClickListener {
+            //dataList = adapter.cords
+            //createFile(this)
+            //FileOutputStream("cordData.csv").apply{writeCsv(adapter.cords)}
+        }
     }
+
+
+
+    private fun MutableList<Cord>.toCsvString(): String{
+        val header = "Latitude,Longitude\n"
+        return this.joinToString(separator = "", prefix = header) { cord ->
+            "${cord.id},${cord.text}\n"
+        }
+    }
+
+    private fun createFile(activity: Activity){
+        val intent = Intent(Intent.ACTION_CREATE_DOCUMENT).apply{
+            addCategory(Intent.CATEGORY_OPENABLE)
+            type = "text/csv"
+            putExtra(Intent.EXTRA_TITLE, "cordData.csv")
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == 101 && resultCode == Activity.RESULT_OK){
+            data?.data?.also{uri ->
+                writeCsvToFile(uri)
+            }
+        }
+    }
+
+    private fun writeCsvToFile(uri: Uri){
+        val contentResolver = applicationContext.contentResolver
+        contentResolver.openFileDescriptor(uri, "w")?.use{ parcelFileDescriptor->
+            FileOutputStream(parcelFileDescriptor.fileDescriptor).bufferedWriter().use{
+                it.write(getCsvData())
+            }
+        }
+    }
+
+    private fun getCsvData(): String {
+        return dataList.toCsvString()
+    }
+
 }
